99c7cf68c4f729e4b3d8603172999a08d4e9eefc
Slash10 <Slash10@macintosh.home>
2020-03-20 21:12:40 +0100

    first commit

---
+++ src/test/java/TestRun.java
@@ -0,0 +1,37 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.aspectj.lang.annotation.Before;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestRun extends BaseClass{
+
+    public static AndroidDriver<AndroidElement> driver; 
+    
+    @BeforeTest
+    public void setup() throws MalformedURLException {
+            File appDir = new File("src/main/resources");
+            File app = new File(appDir, "original.apk");
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
+            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "yassine_emulator");
+            capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+            driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @Test
+    public void test01() {
+        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+        driver.findElementById("android:id/checkbox").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='WiFi settings']").click();
+        driver.findElementByClassName("android.widget.EditText").sendKeys("test edit text");
+        driver.findElementById("android:id/button1").click();
+    }
+}
